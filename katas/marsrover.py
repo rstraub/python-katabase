c8747cb1cea5ef54eda486a52e6077c01c6d2735
Roy Straub <roy@codecraftr.nl>
2024-10-19 21:25:36 +0200

    Uses constants for directions

--- katas/marsrover.py
+++ katas/marsrover.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, replace
+from enum import Enum, auto
 from typing import List
 
 
@@ -8,10 +9,17 @@ class MarsRover:
     y: int
     direction: str
 
+    _NORTH = "N"
+    _SOUTH = "S"
+    _EAST = "E"
+    _WEST = "W"
+
+    _FORWARDS = "f"
+
     def move(self, commands: List[str]) -> "MarsRover":
         first_command = commands[0]
 
-        if first_command == "f":
+        if first_command == self._FORWARDS:
             new_coordinates = self.move_forwards()
         else:
             new_coordinates = self.move_backwards()
@@ -19,25 +27,25 @@ class MarsRover:
         return replace(self, y=new_coordinates.y, x=new_coordinates.x)
 
     def move_forwards(self) -> "Coordinate":
-        if self.direction == "N":
+        if self.direction == self._NORTH:
             return Coordinate(self.x, self.y + 1)
-        elif self.direction == "S":
+        elif self.direction == self._SOUTH:
             return Coordinate(self.x, self.y - 1)
-        elif self.direction == "W":
+        elif self.direction == self._WEST:
             return Coordinate(self.x - 1, self.y)
-        elif self.direction == "E":
+        elif self.direction == self._EAST:
             return Coordinate(self.x + 1, self.y)
         else:
             raise ValueError("unknown direction!")
 
     def move_backwards(self) -> "Coordinate":
-        if self.direction == "N":
+        if self.direction == self._NORTH:
             return Coordinate(self.x, self.y - 1)
-        elif self.direction == "S":
+        elif self.direction == self._SOUTH:
             return Coordinate(self.x, self.y + 1)
-        elif self.direction == "W":
+        elif self.direction == self._WEST:
             return Coordinate(self.x + 1, self.y)
-        elif self.direction == "E":
+        elif self.direction == self._EAST:
             return Coordinate(self.x - 1, self.y)
         else:
             raise ValueError("unknown direction!")
